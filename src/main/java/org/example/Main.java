2ac73014cf6a99ddf170d4e8b064a56c8d917558
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-02-01 09:06:23 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        HTag hTag = new HTag(1);
+        hTag.setText("Jeg er et h tag");
+
+        System.out.println(hTag);
+
+
+    }
+}
